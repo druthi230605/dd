12741be3ca7bc7b031a55e25041d90c9f6ea464e
druthi230605 <druthi2365@gmail.com>
2024-12-27 22:11:11 +0530

    Add files via upload

---
+++ binary.java
@@ -0,0 +1,21 @@
+public class binary {
+    public static void main (String[] args)
+    {
+        //int a=130;
+        //int i;
+        //int div;
+
+        //for(i=0;i<7;i++)
+        //{
+            //div=a/2;
+            //System.out.println(a%2);
+            //a=div;
+            
+        //}
+        //System.out.println(a);
+        int i=138;
+        byte b=(byte)i;
+        System.out.println("b="+b);
+    }
+    
+}
